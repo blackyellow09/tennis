596388c944401f112f0b9386f47c6de894ccd2c3
Sasa <s.neid@gmx.de>
2016-01-06 15:25:53 +0100

    vaadin test

--- src/main/java/de/blackyellow/tennis/menu/Userauswahl.java
+++ src/main/java/de/blackyellow/tennis/menu/Userauswahl.java
@@ -1,20 +1,31 @@
 package de.blackyellow.tennis.menu;
 
+import java.util.Iterator;
+
+import javax.servlet.annotation.WebServlet;
+
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.*;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Label;
 
 /**
  * Servlet implementation class Userauswahl
  */
 //@WebServlet(description = "Hauptmenü der Anwendung, Auswahl des Kunden", urlPatterns = { "/Userauswahl" })
 public class Userauswahl extends UI {
-	private static final long serialVersionUID = 1L;
-       
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5514058771932638536L;
+
 	@Override
-	protected void init(VaadinRequest request) {
-		VerticalLayout view = new VerticalLayout();
-        view.addComponent(new Label("Hello this is Vaadin!"));
+    protected void init(VaadinRequest request) {
+        VerticalLayout view = new VerticalLayout();
+        view.addComponent(new Label("Hello Vaadin!"));
         setContent(view);
-	}
+    }
+
 
 }
