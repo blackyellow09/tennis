cfaf8c5c5f03986f8f759ddb9999f7543e69d653
Sasa <s.neid@gmx.de>
2016-01-13 19:00:55 +0100

    Tabsheet

--- src/main/java/de/blackyellow/tennis/menu/UserauswahlViewImpl.java
+++ src/main/java/de/blackyellow/tennis/menu/UserauswahlViewImpl.java
@@ -1,5 +1,6 @@
 package de.blackyellow.tennis.menu;
 
+
 import com.vaadin.data.Container.Filterable;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.data.util.filter.SimpleStringFilter;
@@ -20,7 +21,9 @@ import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.HeaderRow;
 import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
 import de.blackyellow.tennis.bespannung.BespannungsuebersichtView;
@@ -35,21 +38,89 @@ public class UserauswahlViewImpl extends GridLayout implements UserauswahlView, 
 	private static final long serialVersionUID = -1667952908909776359L;
 	private UserauswahlViewListener listeners;
 
+	
+	
 	@Override
 	public void enter(ViewChangeEvent event) {
 		if(getComponentCount() > 0)
 		{
 			removeAllComponents();
 		}
-		setSizeFull();
-		setColumns(2);
+//		setSizeFull();
+		setColumns(3);
 		
-		BeanItemContainer<Kunde> kunden = listeners.getKunden();
-		addSearchBox(kunden);
+		TabSheet tabsheet = new TabSheet();
+		tabsheet.addStyleName(ValoTheme.TABSHEET_FRAMED);
+		tabsheet.addStyleName(ValoTheme.TABSHEET_PADDED_TABBAR);
 		
-		addUserButtons(kunden);
+		tabsheet.addTab(addTabKunden(), "Kunden");
+		tabsheet.addTab(addTabSchlaeger(), "Schläger");
+		tabsheet.addTab(addTabSaiten(), "Saiten");
 		
-        @SuppressWarnings("serial")
+//        addButtonNeuerSchlaeger();
+//        addButtonNeueSaite();
+//        
+//        
+//        addTableKunden();
+//        addTableKunden();
+        addComponent(tabsheet);
+	}
+
+
+	private VerticalLayout addTabKunden() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponent(addButtonNeuePerson());
+		layout.addComponent(addTableKunden());
+		return layout;
+	}
+	
+	private VerticalLayout addTabSchlaeger() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponent(addButtonNeuerSchlaeger());
+		layout.addComponent(addTableKunden());
+		return layout;
+	}
+	
+	private VerticalLayout addTabSaiten() {
+		VerticalLayout layout = new VerticalLayout();
+		layout.addComponent(addButtonNeueSaite());
+		layout.addComponent(addTableKunden());
+		return layout;
+	}
+
+
+	private Button addButtonNeueSaite() {
+		@SuppressWarnings("serial")
+		Button button = new Button("Neue Saite",
+                new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                getUI().getNavigator().navigateTo(NeuePersonView.NEUE_PERSON);
+            }
+        });
+        button.setStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
+        button.setIcon(FontAwesome.USER_PLUS);
+        return button;
+	}
+
+
+	private Button addButtonNeuerSchlaeger() {
+		@SuppressWarnings("serial")
+		Button button = new Button("Neues Schlägermodell",
+                new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                getUI().getNavigator().navigateTo(NeuePersonView.NEUE_PERSON);
+            }
+        });
+        button.setStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
+        button.setIcon(FontAwesome.USER_PLUS);
+        return button;
+	}
+
+
+	private Button addButtonNeuePerson() {
+		@SuppressWarnings("serial")
 		Button button = new Button("Neue Person",
                 new Button.ClickListener() {
             @Override
@@ -59,92 +130,20 @@ public class UserauswahlViewImpl extends GridLayout implements UserauswahlView, 
         });
         button.setStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
         button.setIcon(FontAwesome.USER_PLUS);
-        addComponent(button);
-        setComponentAlignment(button, Alignment.MIDDLE_CENTER);
-        
-        addTableSchlaeger();
+        return button;
 	}
 	
-	private void addSearchBox(BeanItemContainer<Kunde> colKunde) {
-		// Creates a new combobox using an existing container
-        ComboBox combobox = new ComboBox("Personensuche",colKunde);
- 
-        combobox.setInputPrompt("Nach Person suchen");
- 
-        // Sets the combobox to show a certain property as the item caption
-        combobox.setItemCaptionPropertyId(Kunde.NAME);
-        combobox.setItemCaptionMode(ItemCaptionMode.PROPERTY);
- 
-        // Set full width
-        combobox.setWidth(100.0f, Unit.PERCENTAGE);
- 
-        // Set the appropriate filtering mode for this example
-        combobox.setFilteringMode(FilteringMode.CONTAINS);
-        combobox.setImmediate(true);
- 
-        // Disallow null selections
-        combobox.setNullSelectionAllowed(false);
- 
-        addComponent(combobox);
-        
-        // Check if the caption for new item already exists in the list of item
-        // captions before approving it as a new item.
-//        combobox.setNewItemHandler(new NewItemHandler() {
-//            @Override
-//            public void addNewItem(final String newItemCaption) {
-//                boolean newItem = true;
-//                for (final Object itemId : combobox.getItemIds()) {
-//                    if (newItemCaption.equalsIgnoreCase(combobox
-//                            .getItemCaption(itemId))) {
-//                        newItem = false;
-//                        break;
-//                    }
-//                }
-//                if (newItem) {
-//                    // Adds new option
-//                    if (combobox.addItem(newItemCaption) != null) {
-//                        final Item item = combobox.getItem(newItemCaption);
-//                        item.getItemProperty(ExampleUtil.iso3166_PROPERTY_NAME)
-//                                .setValue(newItemCaption);
-//                        combobox.setValue(newItemCaption);
-//                    }
-//                }
-//            }
-//        });
-	}
-
-	public void addUserButtons(BeanItemContainer<Kunde> kunden)
-	{
-		for (int i = 0; i < kunden.size(); i++) {
-			String kundenname = kunden.getIdByIndex(i).getName();
-			final int kundennummer = kunden.getIdByIndex(i).getKundennummer();
-			
-			@SuppressWarnings("serial")
-			Button button = new Button(kundenname,//"User "+i,
-	                new Button.ClickListener() {
-	            @Override
-	            public void buttonClick(ClickEvent event) {
-	                getUI().getNavigator().navigateTo(BespannungsuebersichtView.BESPANNUNGSUEBERSICHT 
-	                		+ "/" + kundennummer);
-	            }
-	        });
-	        button.setStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
-	        button.setIcon(FontAwesome.USER);
-	        addComponent(button);
-	        setComponentAlignment(button, Alignment.MIDDLE_CENTER);
-		}
-	}
 
 	@Override
 	public void addListener(UserauswahlViewListener listener) {
 		listeners = listener;
 	}
 
-	private void addTableSchlaeger() {
+	private Grid addTableKunden() {
 		//Set the data source (IndexedContainer)
-		BeanItemContainer<Kunde> schlaeger = listeners.getKunden();
+		BeanItemContainer<Kunde> kunden = listeners.getKunden();
 		Grid table = new Grid();
-		table.setContainerDataSource(schlaeger);
+		table.setContainerDataSource(kunden);
 		table.setColumns(Kunde.NACHNAME, Kunde.VORNAME);
 		// Set the selection mode
 		table.setSelectionMode(SelectionMode.SINGLE);
@@ -162,7 +161,7 @@ public class UserauswahlViewImpl extends GridLayout implements UserauswahlView, 
 			}
 		});
 		
-		addComponent(table);
+		return table;
 	}
 	
 	private void setColumnFiltering(Grid table, boolean filtered) {
