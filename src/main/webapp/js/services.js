a8a0721f7dfa8b00449b19721945d0c9849ad60f
blackyellow09 <31367037+blackyellow09@users.noreply.github.com>
2017-09-18 21:32:53 +0200

    Update services.js
    neue host-Url

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -3,7 +3,7 @@ angular.module('app.services', [])
 .factory('MyURL', [function(){
 //	return {host: 'http://localhost:8787/tennisApp/'};
 //	return {host: 'http://localhost:8787/ROOT/'};
-	return {host: 'http://tennis-blackyellow.rhcloud.com/'};
+	return {host: 'http://tennis1-tennis-app.a3c1.starter-us-west-1.openshiftapps.com/'};
 				
 }])
 
