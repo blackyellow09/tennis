53e935bbaf2e47dc49de88023c7c6e85c4f4ae0a
sasab <sasab@mobileKaktus.fritz.box>
2018-06-18 18:28:11 +0200

    SaitenServletTest nach refactoring

---
+++ src/main/java/de/blackyellow/tennis/db/SaitenServices.java
@@ -0,0 +1,50 @@
+package de.blackyellow.tennis.db;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.inject.Singleton;
+
+import org.apache.log4j.Logger;
+
+import de.blackyellow.tennis.saite.Saite;
+import de.blackyellow.tennis.util.ErrorConstants;
+
+@Singleton
+public class SaitenServices {
+
+	private static Logger logger = Logger.getLogger(SaitenServices.class);
+	
+	
+	public ArrayList<Saite> liefereSaiten()
+	{
+		Connection connection = DBConnection.getDBConnection();
+		ArrayList<Saite> listSaite = new ArrayList<Saite>();
+		if(connection == null)
+		{
+			return listSaite;
+		}
+		PreparedStatement preparedStatement;
+		try {
+			preparedStatement = connection.prepareStatement("SELECT * FROM saiten;");
+
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				int id = resultSet.getInt(1);
+				String marke = resultSet.getString(2);
+				String bezeichnung = resultSet.getString(3);
+				String typ = resultSet.getString(4);
+				BigDecimal preis = resultSet.getBigDecimal(5);
+				Saite saite = new Saite(id, marke, bezeichnung, typ, preis);
+				listSaite.add(saite);
+			}
+		} catch (SQLException e) {
+			logger.error(ErrorConstants.FEHLER_LIEFERE_SAITEN, e);
+		}
+		return listSaite;
+	}
+}
--- src/test/java/de/blackyellow/tennis/json/SaitenServletTest.java
+++ src/test/java/de/blackyellow/tennis/json/SaitenServletTest.java
@@ -1,70 +1,139 @@
 package de.blackyellow.tennis.json;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 
+import javax.inject.Singleton;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.hk2.api.Factory;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
 import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.grizzly.GrizzlyTestContainerFactory;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.glassfish.jersey.test.jdkhttp.JdkHttpServerTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerException;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.core.Is;
 import org.hamcrest.core.IsEqual;
+import org.json.simple.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.util.reflection.Whitebox;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.sun.research.ws.wadl.Resource;
 
+import de.blackyellow.tennis.db.SaitenServices;
 import de.blackyellow.tennis.saite.Saite;
 
-public class SaitenServletTest extends JerseyTest{
+public class SaitenServletTest extends JerseyTest{//FunctionalTest{
 
 	SaitenServlet underTest = spy(SaitenServlet.class);
-
+	
+	@Mock
+	SaitenServlet servletMock;
+	
+	@Mock
+	SaitenServices saitenServices;
+	
 	@Test
 	public void wennAlleSaitenGesucht_dannLiefereAlleSaiten() {
 		ArrayList<Saite> colSaiten = new ArrayList<>();
 		colSaiten.add(createSaite());
 		doReturn(colSaiten).when(underTest).liefereSaiten();
 		
-		String alleSaiten = underTest.getAlleSaiten();
+		Response response = underTest.getAlleSaiten();
+		String entity = response.getEntity().toString();
+		List<Saite> alleSaiten = new Gson().fromJson(entity, new TypeToken<List<Saite>>(){}.getType());
 		
-		assertThat(alleSaiten, containsString("Solinco"));
+		assertThat(alleSaiten, contains(saite("Solinco Hyper")));
 	}
-	
-	@Override
-    protected Application configure() {
-		enable(TestProperties.LOG_TRAFFIC);
-        return new ResourceConfig(SaitenServlet.class);
-    }
 	
 	@Test
 	public void testName() throws Exception {
-		String response = target("saiten/alle").request().accept(MediaType.APPLICATION_JSON).get(String.class);
-		
-		assertThat(response, containsString("Saskia Neid"));
-//		assertThat(response.getContentType(), is(MediaType.APPLICATION_JSON));
-//		assertThat(response.getHeader("Access-Control-Allow-Origin"), is("http://tennis.blackyellow.de"));
-	}
+		ArrayList<Saite> colSaiten = new ArrayList<>();
+		colSaiten.add(createSaite());
+		when(saitenServices.liefereSaiten()).thenReturn(colSaiten);
 	
-//	@Test
-//	public void testMitClient() throws Exception {
-//		HttpUriRequest request = new HttpGet("https://localhost/tennis/saiten/alle");
-//		HttpResponse response = HttpClientBuilder.create().build().execute(request);
-//		assertThat(ContentType.getOrDefault(response.getEntity()).getMimeType(), is(MediaType.APPLICATION_JSON));
-//		assertThat(response.getFirstHeader("Access-Control-Allow-Origin"), is("http://tennis.blackyellow.de"));
-//	}
+		WebTarget targetUpdated = target("/saiten/alle");
+	    Response response = targetUpdated.request().accept(MediaType.APPLICATION_JSON).get();
+	    
+		List<Saite> readEntity = response.readEntity(new GenericType<List<Saite>>(){});
+		assertThat(readEntity, contains(saite("Solinco Hyper")));
+		assertThat(response.getMediaType().toString(), is(MediaType.APPLICATION_JSON));
+		assertThat(response.getHeaderString("Access-Control-Allow-Origin"), is("http://tennis.blackyellow.de"));
+	}
 
+	private Matcher<Saite> saite(String string) {
+		return new FeatureMatcher<Saite, String>(containsString(string), "Saite", "Saiten") {
+
+			@Override
+			protected String featureValueOf(Saite actual) {
+				return actual.getName().toString();
+			}
+		};
+	}
+
+	@Override
+		protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
+			return new GrizzlyWebTestContainerFactory();
+		}
+	
+	@Override
+		protected DeploymentContext configureDeployment() {
+		MockitoAnnotations.initMocks(this);
+		enable(TestProperties.LOG_TRAFFIC);
+        enable(TestProperties.DUMP_ENTITY);
+			ResourceConfig resourceConfig = new ResourceConfig(SaitenServlet.class);
+			resourceConfig.register(JacksonFeature.class);
+			resourceConfig.register(JacksonJsonProvider.class);
+			resourceConfig.register(new AbstractBinder() {
+
+				@Override
+				protected void configure() {
+					bind(saitenServices).to(SaitenServices.class).in(Singleton.class);
+				}
+			});
+			return ServletDeploymentContext.forServlet(new ServletContainer(resourceConfig))
+					.build();
+		}
+	
 	protected Saite createSaite() {
 		return new Saite(1, "Solinco", "Hyper", "Poly", BigDecimal.TEN);
 	}
